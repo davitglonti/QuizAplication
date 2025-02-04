8e2b627a2745a700bf04e757d497edda278179d3
davitglonti <101289593+davitglonti@users.noreply.github.com>
2025-02-04 11:04:53 +0400

    Update
     the file didn't upload correctly so I had to manually change the file

--- App.js
+++ App.js
@@ -110,6 +110,7 @@ export default function App() {
   );
 }
 
+
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: "center", padding: 20 },
   title: { fontSize: 20, fontWeight: "bold", textAlign: "center", marginBottom: 20 },
